1591b88557a162eb5fee6f4e907553ac8f6a180c
Jesse Lustig <136506002+dogbless@users.noreply.github.com>
2023-10-23 23:29:37 +0200

    Add files via upload

---
+++ stopwatch-app/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stopwatch_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StopWatch stopWatch = new StopWatch();
+            stopWatch.start();
+        }
+    }
+}
---
+++ stopwatch-app/StopWatch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace stopwatch_app
+{
+    class StopWatch
+    {
+        public void start()
+        {
+            Console.WriteLine("Would you like to start the stopwatch y/n");
+            var userInput = Console.ReadLine();
+            if (userInput == "y")
+            {
+                DateTime startTimer = DateTime.Now;
+                Console.WriteLine("type stop when you would like to stop the stopwatch");
+                var userStop = Console.ReadLine().ToLower();
+                if (userStop == "stop")
+                {
+                    DateTime nowTime = DateTime.Now;
+                    TimeSpan timeDiff = nowTime - startTimer;
+                    Console.WriteLine(timeDiff.TotalSeconds + " seconds have passed");
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("ok goodbye");
+            }
+
+
+        }
+    }
+}
